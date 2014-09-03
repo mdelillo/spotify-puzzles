4b27233d088712f11add216ca17b02aed23aea24
mark <bender-git.med@delillo.org>
2014-09-03 00:56:24 -0400

    [Mark] Implementing main method

--- zipf-song/src/main/java/com/markdelillo/Main.java
+++ zipf-song/src/main/java/com/markdelillo/Main.java
@@ -1,6 +1,22 @@
 package com.markdelillo;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int tracksOnAlbum = scanner.nextInt();
+        int topTracksToDisplay = scanner.nextInt();
+
+        Album album = new Album();
+        for (int i = 0 ; i < tracksOnAlbum; i++) {
+            int playCount = scanner.nextInt();
+            String title = scanner.nextLine().trim();
+            album.addTrack(new Track(title, playCount));
+        }
+
+        for (String title : album.getTopTrackTitles(topTracksToDisplay)) {
+            System.out.println(title);
+        }
     }
 }
